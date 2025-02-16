10efd966167ba762a13f5d3deacac9e1349d0772
Meda <ahmedhemida121@gmail.com>
2025-02-17 00:57:15 +0200

    bash script for testing

---
+++ bash-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Print the current date and time
+echo "Current date and time: $(date)"
+
+# Print a message indicating the script has run successfully
+echo "Script executed successfully!"
